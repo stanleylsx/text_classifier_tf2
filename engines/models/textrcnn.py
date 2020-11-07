bcdde641fd53c91f986b673a2af623b08576f3d4
lishouxian <gzlishouxian@gmail.com>
2020-11-07 11:01:41 +0800

    code

--- engines/models/textrcnn.py
+++ engines/models/textrcnn.py
@@ -13,30 +13,31 @@ class TextRCNN(tf.keras.Model, ABC):
     """
     TextCNN模型
     """
+
     def __init__(self, seq_length, num_classes, hidden_dim, embedding_dim):
         super(TextRCNN, self).__init__()
         self.seq_length = seq_length
         self.hidden_dim = hidden_dim
         self.embedding_dim = embedding_dim
-        self.bilstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(self.hidden_dim, return_sequences=True))
-        # self.pool1 = tf.keras.layers.MaxPooling2D(pool_size=[seq_length-2+1, 1], padding='valid')
+        self.forward = tf.keras.layers.LSTM(self.hidden_dim, return_sequences=True)
+        self.backward = tf.keras.layers.LSTM(self.hidden_dim, return_sequences=True, go_backwards=True)
+        self.max_pool = tf.keras.layers.GlobalMaxPool1D()
         self.dropout = tf.keras.layers.Dropout(classifier_config['droupout_rate'], name='dropout')
-        self.dense = tf.keras.layers.Dense(2 * self.hidden_dim + self.embedding_dim, activation='tanh')
-        self.dense = tf.keras.layers.Dense(num_classes,
-                                           activation='softmax',
-                                           kernel_regularizer=tf.keras.regularizers.l2(0.1),
-                                           bias_regularizer=tf.keras.regularizers.l2(0.1),
-                                           name='dense')
+        self.dense1 = tf.keras.layers.Dense(2 * self.hidden_dim + self.embedding_dim, activation='tanh')
+        self.dense2 = tf.keras.layers.Dense(num_classes,
+                                            activation='softmax',
+                                            kernel_regularizer=tf.keras.regularizers.l2(0.1),
+                                            bias_regularizer=tf.keras.regularizers.l2(0.1),
+                                            name='dense')
         self.flatten = tf.keras.layers.Flatten(data_format='channels_last', name='flatten')
 
-    # @tf.function
+    @tf.function
     def call(self, inputs, training=None):
-        bilstm_outputs = self.bilstm(inputs)
-        concat_outputs = tf.keras.layers.concatenate([bilstm_outputs, inputs], axis=-1, name='concatenate')
-        print('asd')
-        # fc_input =
-        # fc_output = tf.tanh(concat_outputs)
-        # flatten_outputs = self.flatten(concat_outputs)
-        # dropout_outputs = self.dropout(flatten_outputs, training)
-        # outputs = self.dense(dropout_outputs)
-        # return outputs
+        left_embedding = self.forward(inputs)
+        right_embedding = self.backward(inputs)
+        concat_outputs = tf.keras.layers.concatenate([left_embedding, inputs, right_embedding], axis=-1)
+        dropout_outputs = self.dropout(concat_outputs, training)
+        fc_outputs = self.dense1(dropout_outputs)
+        pool_outputs = self.max_pool(fc_outputs)
+        outputs = self.dense2(pool_outputs)
+        return outputs
