7e4f4fa063967cfc4ef519c263bc8ff379319d28
unknown <gzlishouxian@game.ntes>
2021-03-17 19:35:52 +0800

    修改了下focalloss函数，使其可以用于多分类

--- engines/utils/focal_loss.py
+++ engines/utils/focal_loss.py
@@ -8,14 +8,36 @@ import tensorflow as tf
 
 
 class FocalLoss(tf.keras.losses.Loss):
-    def __init__(self, gamma=2.0, alpha=0.25):
+    """
+    focal loss for multi-class classification
+    fl(pt) = -alpha*(1-pt)^(gamma)*log(pt)
+    :param alpha:
+    :param gamma:
+    :param epsilon:
+    """
+    def __init__(self, gamma=2.0, alpha=0.25, epsilon=1.e-9):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
         self.alpha = alpha
+        # To avoid divided by zero
+        self.epsilon = epsilon
 
     def call(self, y_true, y_pred):
-        y_pred = tf.nn.softmax(y_pred, axis=-1)
-        y_true = tf.cast(y_true, tf.float32)
-        loss = -y_true * tf.math.pow(1 - y_pred, self.gamma) * tf.math.log(y_pred)
-        loss = tf.reduce_sum(loss, axis=1)
+        """
+        :param y_true: ground truth one-hot vector shape of [batch_size, nb_class]
+        :param y_pred: prediction after softmax shape of [batch_size, nb_class]
+        :return:
+        """
+        y_pred = tf.add(y_pred, self.epsilon)
+        # Cross entropy
+        ce = -y_true * tf.math.log(y_pred)
+        # Not necessary to multiply y_true(cause it will multiply with CE which has set unconcerned index to zero ),
+        # but refer to the definition of p_t, we do it
+        weight = tf.math.pow(1 - y_pred, self.gamma) * y_true
+        # Now fl has a shape of [batch_size, nb_class]
+        # alpha should be a step function as paper mentioned, but it doesn't matter like reason mentioned above
+        # (CE has set unconcerned index to zero)
+        # alpha_step = tf.where(y_true, alpha*np.ones_like(y_true), 1-alpha*np.ones_like(y_true))
+        fl = ce * weight * self.alpha
+        loss = tf.reduce_sum(fl, axis=1)
         return loss
