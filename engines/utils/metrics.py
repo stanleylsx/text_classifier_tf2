2c3596963f7aee20a87b80efdbeefda999d094cc
lishouxian <gzlishouxian@gmail.com>
2021-10-13 10:27:48 +0800

    Update metrics.py

--- engines/utils/metrics.py
+++ engines/utils/metrics.py
@@ -6,6 +6,7 @@
 # @Software: PyCharm
 from sklearn import metrics
 from config import classifier_config
+import numpy as np
 
 
 def cal_metrics(y_true, y_pred):
@@ -15,8 +16,9 @@ def cal_metrics(y_true, y_pred):
     y_true = y_true.astype(int)
     y_pred = y_pred.astype(int)
     average = classifier_config['metrics_average']
-    precision = metrics.precision_score(y_true, y_pred, average=average)
-    recall = metrics.recall_score(y_true, y_pred, average=average)
-    f1 = metrics.f1_score(y_true, y_pred, average=average)
-    each_classes = metrics.classification_report(y_true, y_pred, output_dict=True)
+    precision = metrics.precision_score(y_true, y_pred, average=average, labels=np.unique(y_pred), zero_division=0)
+    recall = metrics.recall_score(y_true, y_pred, average=average, labels=np.unique(y_pred), zero_division=0)
+    f1 = metrics.f1_score(y_true, y_pred, average=average, labels=np.unique(y_pred), zero_division=0)
+    each_classes = metrics.classification_report(y_true, y_pred, output_dict=True, labels=np.unique(y_pred),
+                                                 zero_division=0)
     return {'precision': precision, 'recall': recall, 'f1': f1}, each_classes
