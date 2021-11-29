62b58507dbd28ccb83d6dae903ec9795234149c2
lishouxian <gzlishouxian@gmail.com>
2021-11-29 15:02:16 +0800

    Update focal_loss.py

--- engines/utils/focal_loss.py
+++ engines/utils/focal_loss.py
@@ -1,40 +1,43 @@
 # -*- coding: utf-8 -*-
-# @Time : 2021/11/26 7:51 下午
+# @Time : 2020/11/25 7:51 下午 
 # @Author : lishouxian
 # @Email : gzlishouxian@gmail.com
-# @File : r_drop_loss.py
+# @File : focal_loss.py 
 # @Software: PyCharm
 import tensorflow as tf
-from config import classifier_config
-from engines.utils.focal_loss import FocalLoss
 
 
-class RDropLoss:
+class FocalLoss(tf.keras.losses.Loss):
     """
-    r drop loss
+    focal loss for multi-class classification
+    fl(pt) = -alpha*(1-pt)^(gamma)*log(pt)
+    :param alpha:
+    :param gamma:
+    :param epsilon:
     """
-    def __init__(self):
-        super(RDropLoss, self).__init__()
-        self.alpha = 4
-        self.kl_loss = tf.keras.losses.KLDivergence(reduction=tf.keras.losses.Reduction.NONE, name='kl_divergence')
-        self.loss = FocalLoss() if classifier_config['use_focal_loss'] else tf.keras.losses.CategoricalCrossentropy()
+    def __init__(self, gamma=2.0, alpha=0.25, epsilon=1.e-9):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        # To avoid divided by zero
+        self.epsilon = epsilon
 
-    def calculate_loss(self, p, q, y_true):
-        loss_1 = self.loss(y_true=y_true, y_pred=p)
-        loss_2 = self.loss(y_true=y_true, y_pred=q)
-        loss = 0.5 * (loss_1 + loss_2)
-        loss = tf.reduce_mean(loss)
-
-        kl_loss_1 = self.kl_loss(p, q)
-        kl_loss_2 = self.kl_loss(q, p)
-        kl_loss = 0.5 * (kl_loss_1 + kl_loss_2)
-        kl_loss = tf.reduce_mean(kl_loss)
-
-        loss = loss + self.alpha * kl_loss
+    def call(self, y_true, y_pred):
+        """
+        :param y_true: ground truth one-hot vector shape of [batch_size, nb_class]
+        :param y_pred: prediction after softmax shape of [batch_size, nb_class]
+        :return:
+        """
+        y_pred = tf.add(y_pred, self.epsilon)
+        # Cross entropy
+        ce = -y_true * tf.math.log(y_pred)
+        # Not necessary to multiply y_true(cause it will multiply with CE which has set unconcerned index to zero ),
+        # but refer to the definition of p_t, we do it
+        weight = tf.math.pow(1 - y_pred, self.gamma) * y_true
+        # Now fl has a shape of [batch_size, nb_class]
+        # alpha should be a step function as paper mentioned, but it doesn't matter like reason mentioned above
+        # (CE has set unconcerned index to zero)
+        # alpha_step = tf.where(y_true, alpha*np.ones_like(y_true), 1-alpha*np.ones_like(y_true))
+        fl = ce * weight * self.alpha
+        loss = tf.reduce_sum(fl, axis=1)
         return loss
-
-
-
-
-
-
