5b67ad76c8f309e83c334e4a62f66f0a972c8206
lishouxian <gzlishouxian@gmail.com>
2021-11-29 15:12:00 +0800

    Update r_drop_loss.py

--- engines/utils/r_drop_loss.py
+++ engines/utils/r_drop_loss.py
@@ -5,6 +5,8 @@
 # @File : r_drop_loss.py
 # @Software: PyCharm
 import tensorflow as tf
+from config import classifier_config
+from engines.utils.focal_loss import FocalLoss
 
 
 class RDropLoss:
@@ -15,24 +17,18 @@ class RDropLoss:
         super(RDropLoss, self).__init__()
         self.alpha = 4
         self.kl_loss = tf.keras.losses.KLDivergence(reduction=tf.keras.losses.Reduction.NONE, name='kl_divergence')
-        self.ce_loss = tf.keras.losses.CategoricalCrossentropy()
+        self.loss = FocalLoss() if classifier_config['use_focal_loss'] else tf.keras.losses.CategoricalCrossentropy()
 
     def calculate_loss(self, p, q, y_true):
-        ce_loss_1 = self.ce_loss(y_true=y_true, y_pred=p)
-        ce_loss_2 = self.ce_loss(y_true=y_true, y_pred=q)
-        ce_loss = 0.5 * (ce_loss_1 + ce_loss_2)
-        ce_loss = tf.reduce_mean(ce_loss)
+        loss_1 = self.loss(y_true=y_true, y_pred=p)
+        loss_2 = self.loss(y_true=y_true, y_pred=q)
+        loss = 0.5 * (loss_1 + loss_2)
+        loss = tf.reduce_mean(loss)
 
         kl_loss_1 = self.kl_loss(p, q)
         kl_loss_2 = self.kl_loss(q, p)
         kl_loss = 0.5 * (kl_loss_1 + kl_loss_2)
         kl_loss = tf.reduce_mean(kl_loss)
 
-        loss = ce_loss + self.alpha * kl_loss
+        loss = loss + self.alpha * kl_loss
         return loss
-
-
-
-
-
-
