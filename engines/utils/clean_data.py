d2b1351cc5600447e2a028555513b47b78081ee1
lishouxian <gzlishouxian@gmail.com>
2022-05-25 17:20:54 +0800

    Update clean_data.py

--- engines/utils/clean_data.py
+++ engines/utils/clean_data.py
@@ -8,7 +8,7 @@ import re
 
 
 def filter_word(raw_word):
-    if raw_word in ['\t', '']:
+    if raw_word.strip() in ['\t', '']:
         return False
     if not re.search(r'^[\u4e00-\u9fa5_a-zA-Z\d]+$', raw_word):
         return False
@@ -17,7 +17,7 @@ def filter_word(raw_word):
 
 
 def filter_char(char, remove_sp=True):
-    if char in ['\t', '']:
+    if char.strip() in ['\t', '']:
         return False
     if remove_sp:
         if re.search(r'[\u4e00-\u9fa5_a-zA-Z\d]', char):
