c03480fe697580389ca81837608b09a43b400ba3
lishouxian <gzlishouxian@gmail.com>
2022-05-25 17:01:10 +0800

    Update clean_data.py

--- engines/utils/clean_data.py
+++ engines/utils/clean_data.py
@@ -8,14 +8,21 @@ import re
 
 
 def filter_word(raw_word):
-    if not re.search(r'^[\u4e00-\u9fa5_a-zA-Z0-9]+$', raw_word):
+    if raw_word in ['\t', '']:
+        return False
+    if not re.search(r'^[\u4e00-\u9fa5_a-zA-Z\d]+$', raw_word):
         return False
     else:
         return True
 
 
-def filter_char(char):
-    if not re.search(r'[\u4e00-\u9fa5_a-zA-Z0-9]', char):
+def filter_char(char, remove_sp=True):
+    if char in ['\t', '']:
         return False
+    if remove_sp:
+        if re.search(r'[\u4e00-\u9fa5_a-zA-Z\d]', char):
+            return True
+        else:
+            return False
     else:
-        return True
+        return False
