778ecabe07048c0f88e996d0033b95d90e52c4b3
lishouxian <gzlishouxian@gmail.com>
2022-10-20 11:22:00 +0800

    Update metrics.py

--- engines/utils/metrics.py
+++ engines/utils/metrics.py
@@ -4,7 +4,7 @@
 # @Email : gzlishouxian@gmail.com
 # @File : metrics.py 
 # @Software: PyCharm
-from sklearn import metrics
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support, classification_report
 from config import classifier_config
 import numpy as np
 
@@ -16,9 +16,8 @@ def cal_metrics(y_true, y_pred):
     y_true = y_true.astype(int)
     y_pred = y_pred.astype(int)
     average = classifier_config['metrics_average']
-    precision = metrics.precision_score(y_true, y_pred, average=average, labels=np.unique(y_pred), zero_division=0)
-    recall = metrics.recall_score(y_true, y_pred, average=average, labels=np.unique(y_pred), zero_division=0)
-    f1 = metrics.f1_score(y_true, y_pred, average=average, labels=np.unique(y_pred), zero_division=0)
-    each_classes = metrics.classification_report(y_true, y_pred, output_dict=True, labels=np.unique(y_pred),
+    precision, recall, f1, _ = precision_recall_fscore_support(y_true, y_pred, average=average, zero_division=0)
+    accuracy = accuracy_score(y_true, y_pred)
+    each_classes = classification_report(y_true, y_pred, output_dict=True, labels=np.unique(y_pred),
                                                  zero_division=0)
-    return {'precision': precision, 'recall': recall, 'f1': f1}, each_classes
+    return {'precision': precision, 'recall': recall, 'f1': f1, 'accuracy': accuracy}, each_classes
